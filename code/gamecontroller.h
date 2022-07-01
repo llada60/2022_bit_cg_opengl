02e95a34fd7b4e53ff918e67462255fb76dff2f9
ll <572192783@qq.com>
2022-07-01 10:39:31 +0800

    main_v2

---
+++ code/gamecontroller.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
+
+#include "mesh.h"
+#include "shader.h"
+#include "camera.h"
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+// settings
+const unsigned int SCR_WIDTH = 1280;
+const unsigned int SCR_HEIGHT = 720;
+
+//camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+static float lastX = (float)SCR_WIDTH / 2.0;
+static float lastY = (float)SCR_HEIGHT / 2.0;
+static bool firstMouse = true;
+
+// timing
+static float deltaTime = 0.0f;
+static float lastFrame = 0.0f;
+
+void updateGameController(GLFWwindow* window)
+{
+	float currentFrame = glfwGetTime();
+	deltaTime = currentFrame - lastFrame;
+	lastFrame = currentFrame;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
--- code/main.cpp
+++ code/main.cpp
@@ -12,55 +12,16 @@
 #include "model.h"
 #include "texture.h"
 #include "LoadPlyModel.h"
+#include "gamecontroller.h"
 #include <iostream>
 #include <irrklang/irrKlang.h>
+
 using namespace irrklang;
 
 ISoundEngine* SoundEngine = createIrrKlangDevice();
-//
-// settings
-const unsigned int SCR_WIDTH = 1280;
-const unsigned int SCR_HEIGHT = 720;
-bool light = false;
-bool ball = false;
-bool collision = false;
-float rate = 0.01;
-int rateCount = 0;
-bool visible = true;
-
-// camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
-float lastX = (float)SCR_WIDTH / 2.0;
-float lastY = (float)SCR_HEIGHT / 2.0;
-bool firstMouse = true;
-
-// timing
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
 
 // material
-std::vector<vector<float> > material4 = { {0.25, 0.20725, 0.20725}, {169.0/255, 169.0 / 255, 169.0 / 255}, {0.296648, 0.296648, 0.296648}};
-std::vector<vector<float> > material3 = { {0.19225,0.19225,0.19225}, {0.50754,0.50754,0.50754}, {0.508273,0.508273,0.508273} };
-std::vector<vector<float> > material2 = { {0.192250f, 0.192250f, 0.192250f}, {0.667540f, 0.557540f, 0.337540f}, {0.508273f, 0.508273f, 0.508273f} };
-std::vector<vector<float> > material1 = { {0.247250f, 0.199500f, 0.074500f}, {0.751640f, 0.606480f, 0.226480f}, {0.628281f, 0.555802f, 0.556065f} };
-
 std::vector<float>Shininess = {11.264, 51.2, 61.2, 171.200001f };
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow* window)
-{
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, true);
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
-}
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
@@ -71,32 +32,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 	glViewport(0, 0, width, height);
 }
 
-// glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
 
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-	lastX = xpos;
-	lastY = ypos;
-
-	camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	camera.ProcessMouseScroll(yoffset);
-}
 
 //float inter(float a, float b, float time, float interval);
 
@@ -140,30 +76,11 @@ int main()
 
 	// build and compile shaders
 	// -------------------------
-
 	Shader shader("resources/shader/normal_mapping.vs", "resources/shader/normal_mapping.fs");
 	Shader skyboxShader("resources/shader/skybox.vs", "resources/shader/skybox.fs");
 	Skybox skybox(0);
-	//Shader modelShader("model.vs", "model.fs");
 	Shader modelShader("resources/shader/light_casters.vs", "resources/shader/light_casters.fs");
-	//Model skull("resources/objects/skull/skull.obj");
-	Shader shaderBall("resources/shader/pbr.vs", "resources/shader/pbr.fs");
-	//Shader skyboxShader1("resources/shader/skybox1.vs", "resources/shader/skybox1.fs");
-	//Skybox skybox1(1);
-	Shader floorShader("resources/shader/blending.vs", "resources/shader/blending.fs");
 	Shader lightingShader("resources/shader/light_casters.vs", "resources/shader/light_casters.fs");
-	//Shader reconShader("resources/shader/recon.vs", "resources/shader/recon.fs");
-	Shader lightingShader1("resources/shader/light_casters1.vs", "resources/shader/light_casters1.fs");
-
-	floorShader.use();
-	floorShader.setInt("texture1", 0);
-
-	shaderBall.use();
-	shaderBall.setInt("albedoMap", 0);
-	shaderBall.setInt("normalMap", 1);
-	shaderBall.setInt("metallicMap", 2);
-	shaderBall.setInt("roughnessMap", 3);
-	shaderBall.setInt("aoMap", 4);
 
 	// shader configuration
     // --------------------
@@ -171,51 +88,15 @@ int main()
 	lightingShader.setInt("material.diffuse", 0);
 	lightingShader.setInt("material.specular", 1);
 
-	Model rock("resources/objects/rock/rock.obj");
-	Model bomb("resources/objects/Bomb/Bomb.obj");
-	//Model test("resources/objects/happy_recon/happy_vrip.obj");
-
-	// load PBR material textures
-	// --------------------------
-	unsigned int albedo = loadTexture("resources/textures/pbr/rusted_iron/jin.png");
-	unsigned int albedo1 = loadTexture("resources/textures/pbr/rusted_iron/mu.png");
-	unsigned int albedo2 = loadTexture("resources/textures/pbr/rusted_iron/shui.png");
-	unsigned int albedo3 = loadTexture("resources/textures/pbr/rusted_iron/huo.png");
-	unsigned int albedo4 = loadTexture("resources/textures/pbr/rusted_iron/tu.png");
-	unsigned int normal = loadTexture("resources/textures/pbr/rusted_iron/normal.png");
-	unsigned int metallic = loadTexture("resources/textures/pbr/rusted_iron/metallic.png");
-	unsigned int roughness = loadTexture("resources/textures/pbr/rusted_iron/roughness.png");
-	unsigned int ao = loadTexture("resources/textures/pbr/rusted_iron/ao.png");
-
-	// load textures
-	// -------------
-	unsigned int diffuseMap = loadTexture("resources/textures/book.jpg");
-	unsigned int normalMap = loadTexture("resources/textures/book_normal1.jpg");
-	unsigned int diffuseMap1 = loadTexture("resources/textures/bookLeft.png");
-	unsigned int normalMap1 = loadTexture("resources/textures/bookLeft_normal.jpg");
-	unsigned int diffuseMap2 = loadTexture("resources/textures/bookBottom.png");
-	unsigned int normalMap2 = loadTexture("resources/textures/bookBottom_normal.jpg");
-
-	//floor
-	unsigned int floorTexture = loadTexture("resources/textures/metal.png");
-
-	// shader configuration
-	// --------------------
-	shader.use();
-	shader.setInt("diffuseMap", 0);
-	shader.setInt("normalMap", 1);
+	// model
+	// -----
+	Model spaceship1("resources/objects/spaceship2/spaceship.obj");
 
 	// lighting info
 	// -------------
 	glm::vec3 lightPos(0.5f, 0.5f, 1.3f);
 
-	// shader configuration
-// --------------------
-	lightingShader1.use();
-	lightingShader1.setInt("material.diffuse", 0);
-	lightingShader1.setInt("material.specular", 1);
-
-	// shader configuration
+	// skybox shader configuration
 // --------------------
 	skyboxShader.use();
 	skyboxShader.setInt("skybox", 0);
@@ -233,16 +114,6 @@ int main()
 	int nrColumns = 7;
 	float spacing = 2.5;
 
-	// initialize static shader uniforms before rendering
-	// --------------------------------------------------
-	glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-	shaderBall.use();
-	shaderBall.setMat4("projection", projection);
-
-	/*float startTime = glfwGetTime();
-	float time;
-	float time1 = 0;*/
-	float alpha = 0;
 	// set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
 	float planeVertices[] = {
@@ -298,10 +169,7 @@ int main()
 	{
 		// per-frame time logic
 		// --------------------
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		/*time = currentFrame - startTime; */
+		updateGameController(window);
 
 		// input
 		// -----
@@ -313,185 +181,31 @@ int main()
 		//glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		// draw model
+		lightingShader.use();
+		lightingShader.setVec3("light.position", camera.Position);
+		lightingShader.setVec3("light.direction", camera.Front);
+		lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+		lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
+		lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+		lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
+		lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+		lightingShader.setFloat("light.constant", 1.0f);
+		lightingShader.setFloat("light.linear", 0.09f);
+		lightingShader.setFloat("light.quadratic", 0.032f);
+		lightingShader.setFloat("material.shininess", 2.0f);
 		glm::mat4 view = camera.GetViewMatrix(); 
 		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		lightingShader.setMat4("projection", projection);
+		lightingShader.setMat4("view", view);
 
-		/*if(time > 12 && time < 15)
-			camera.ProcessKeyboard(FORWARD, deltaTime);*/
-		// configure view/projection matrices
-		projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-		view = camera.GetViewMatrix();
-		shader.use();
-		shader.setMat4("projection", projection);
-		shader.setMat4("view", view);
-		// render normal-mapped quad
+		//spaceship
 		model = glm::mat4(1.0f);
-		model = glm::scale(model, glm::vec3(0.7f, 0.7f, 0.7f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * -40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		shader.setMat4("model", model);
-		shader.setVec3("viewPos", camera.Position);
-		shader.setVec3("lightPos", lightPos);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normalMap);
-		renderQuad();
-
-		// render normal-mapped quad
-		model = glm::mat4(1.0f);
-		model = glm::scale(model, glm::vec3(0.7f, 0.7f, 0.7f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * -40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -0.1f)); 
-		shader.setMat4("model", model);
-		shader.setVec3("viewPos", camera.Position);
-		shader.setVec3("lightPos", lightPos);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normalMap);
-		renderQuad();
-
-		// render normal-mapped quad
-		model = glm::mat4(1.0f);
-		model = glm::scale(model, glm::vec3(0.7f, 0.7f, 0.7f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * -40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::translate(model, glm::vec3(-0.44f, 0.0f, -0.05f)); // translate it down so it's at the center of the scene
-		model = glm::rotate(model, glm::radians(90.0f), glm::normalize(glm::vec3(0.0, 1.0, 0.0))); // rotate the quad to show normal mapping from multiple directions
-		shader.setMat4("model", model);
-		shader.setVec3("viewPos", camera.Position);
-		shader.setVec3("lightPos", lightPos);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap1);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normalMap1);
-		renderQuad1();
-
-		// render normal-mapped quad
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * -40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.7f, 0.7f, 0.7f));
-		model = glm::translate(model, glm::vec3(0.44f, 0.0f, -0.05f)); // translate it down so it's at the center of the scene
-		model = glm::rotate(model, glm::radians(90.0f), glm::normalize(glm::vec3(0.0, 1.0, 0.0))); // rotate the quad to show normal mapping from multiple directions
-		shader.setMat4("model", model);
-		shader.setVec3("viewPos", camera.Position);
-		shader.setVec3("lightPos", lightPos);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap2);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normalMap2);
-		renderQuad1();
-
-		// render normal-mapped quad
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * -40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.7f, 0.7f, 0.7f));
-		model = glm::translate(model, glm::vec3(0.0f, 0.64f, -0.05f)); // translate it down so it's at the center of the scene
-		model = glm::rotate(model, glm::radians(90.0f), glm::normalize(glm::vec3(1.0, 0.0, 0.0))); // rotate the quad to show normal mapping from multiple directions
-		shader.setMat4("model", model);
-		shader.setVec3("viewPos", camera.Position);
-		shader.setVec3("lightPos", lightPos);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap2);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normalMap2);
-		renderQuad2();
-
-		// render normal-mapped quad
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * -40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.7f, 0.7f, 0.7f));
-		model = glm::translate(model, glm::vec3(0.0f, -0.64f, -0.05f)); // translate it down so it's at the center of the scene
-		model = glm::rotate(model, glm::radians(90.0f), glm::normalize(glm::vec3(1.0, 0.0, 0.0))); // rotate the quad to show normal mapping from multiple directions
-		shader.setMat4("model", model);
-		shader.setVec3("viewPos", camera.Position);
-		shader.setVec3("lightPos", lightPos);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, diffuseMap2);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normalMap2);
-		renderQuad2();
-
-		// draw ball
-		shaderBall.use();
-		view = camera.GetViewMatrix();
-		shaderBall.setMat4("view", view);
-		shaderBall.setVec3("camPos", camera.Position);
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, albedo);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, normal);
-		glActiveTexture(GL_TEXTURE2);
-		glBindTexture(GL_TEXTURE_2D, metallic);
-		glActiveTexture(GL_TEXTURE3);
-		glBindTexture(GL_TEXTURE_2D, roughness);
-		glActiveTexture(GL_TEXTURE4);
-		glBindTexture(GL_TEXTURE_2D, ao);
-		if (alpha < 1)
-			alpha += 0.0001;
-		//ball1
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(0.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::translate(model, glm::vec3(0.0f, 1.0f, 0.0f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.1f * alpha, 0.1f * alpha, 0.1f * alpha));
-		shaderBall.setMat4("model", model);
-		renderSphere();
-		//ball2
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, albedo1);
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(0.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::translate(model, glm::vec3(-0.95f, 0.31f, 0.0f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.1f * alpha, 0.1f * alpha, 0.1f * alpha));
-		shaderBall.setMat4("model", model);
-		renderSphere();
-		//ball3
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, albedo2);
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(0.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::translate(model, glm::vec3(0.95f, 0.31f, 0.0f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.1f * alpha, 0.1f * alpha, 0.1f * alpha));
-		shaderBall.setMat4("model", model);
-		renderSphere();
-		//ball4
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, albedo3);
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(0.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::translate(model, glm::vec3(0.59f, -0.81f, 0.0f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.1f * alpha, 0.1f * alpha, 0.1f * alpha));
-		shaderBall.setMat4("model", model);
-		renderSphere();
-		//ball5
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, albedo4);
-		model = glm::mat4(1.0f);
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(0.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::translate(model, glm::vec3(-0.59f, -0.81f, 0.0f));
-		model = glm::rotate(model, glm::radians((float)glfwGetTime() * 40.0f), glm::normalize(glm::vec3(1.0, 0.0, 1.0))); // rotate the quad to show normal mapping from multiple directions
-		model = glm::scale(model, glm::vec3(0.1f * alpha, 0.1f * alpha, 0.1f * alpha));
-		shaderBall.setMat4("model", model);
-		renderSphere();
-		// render light source (simply re-render sphere at light positions)
-		// this looks a bit off as we use the same shader, but it'll make their positions obvious and 
-		// keeps the codeprint small.
-		for (unsigned int i = 0; i < sizeof(lightPositions) / sizeof(lightPositions[0]); ++i)
-		{
-			glm::vec3 newPos = lightPositions[i] + glm::vec3(sin(glfwGetTime() * 5.0) * 5.0, 0.0, 0.0);
-			newPos = lightPositions[i];
-			shaderBall.setVec3("lightPositions[" + std::to_string(i) + "]", newPos);
-			shaderBall.setVec3("lightColors[" + std::to_string(i) + "]", lightColors[i]);
-
-			model = glm::mat4(1.0f);
-			model = glm::translate(model, newPos);
-			model = glm::scale(model, glm::vec3(0.5f));
-			shaderBall.setMat4("model", model);
-			//renderSphere();
-		}
+		model = glm::translate(model, glm::vec3(2.0f, -0.5f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));	
+		lightingShader.setMat4("model", model);
+		spaceship1.Draw(lightingShader);
 
 		//draw skybox at last
 		skyboxShader.use();
